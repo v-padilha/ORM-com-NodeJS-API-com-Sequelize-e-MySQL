8a4cd86abb82f55d4091247925deb316149b9043
Vinycius Osadczuk Padilha <vinyosadczuk@hotmail.com>
2022-04-12 13:48:47 -0300

    Inserindo novas rotas

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,17 @@
 const bodyParser = require('body-parser')
-const pessoas = require('./pessoasRoute')
 
+const pessoas = require('./pessoasRoute')
+//adicionamos as rotas de niveis e turmas
+const niveis = require('./niveisRoute')
+const turmas = require('./turmasRoute')
+
+//adicionamos as instâncias de níveis e turmas
+//e refatoramos um pouco a função
 module.exports = app => {
-    app.use(bodyParser.json())
-    app.use(pessoas)
-}
+ app.use(
+   bodyParser.json(),
+   pessoas,
+   niveis,
+   turmas
+   )
+ }
